612e315be66daa4c65e877917aab8dbf85263274
JustiTheGreat <justinian.zaharia@gmail.com>
2021-11-04 14:11:14 +0200

    first commit

---
+++ build/CMakeFiles/iep-laboratory-homeworks.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/iep-laboratory-homeworks.dir/src/main.cpp.o"
+  "iep-laboratory-homeworks"
+  "iep-laboratory-homeworks.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/iep-laboratory-homeworks.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
